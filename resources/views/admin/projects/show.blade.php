66e7bc9c6457d91d09a3899539c438cb03ce5baf
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-03-14 19:57:27 +0100

    implementata la funzione per l'upload di files

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.app')
 @vite(['resources/js/app.js'])
 @section('content')
-    <div class="container mt-5 ">
-        <h1>{{$project->name_project}}</h1>
-        <h3>{{$project->description}}</h3>
+    <div class="container mt-5 d-flex justify-content-between align-items-center show">
+        <div>
+            <h2>{{$project->name_project}}</h2>
+            <p>{{$project->description}}</p>
+            <a href="{{route('admin.projects.edit', $project)}}"  class="btn btn-success">modifica</a>
+        </div>
+        <img src="{{ asset('storage/' . $project->image) }}" class="preview"> 
     </div>
+
 @endsection
